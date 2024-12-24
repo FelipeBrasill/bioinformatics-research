5da7957d3c319a697685aae1bc2d06c39daf7abd
Felip <felipebrasil567@gmail.com>
2024-12-24 12:04:56 -0300

    basecounting.py

--- biofolder/basecounting.py
+++ biofolder/basecounting.py
@@ -1,1 +1,18 @@
+blist=input("give the genetic code data at most 1000nt")
+A=0
+C=0
+T=0
+G=0
+if len(blist)>1000:
+    print("sorry,this program are incapable of count more than 1000nt")
+for indice in range(len(blist)):
+    if blist[indice]=='A':
+        A=A+1
+    if blist[indice]=='C':
+        C=C+1
+    if blist[indice]=='T':
+        T=T+1
+    if blist[indice]=='G':
+        G=G+1
+print(A,"",C,"",G,"",T)
 
